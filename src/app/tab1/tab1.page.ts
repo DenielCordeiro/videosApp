c34a0640b3c783528c5fd6f6d22ff362d81371a9
Daniel de Souza Cordeiro <dcordeiro962@gmail.com>
2021-12-01 16:34:44 -0300

    adicionei o alert

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,30 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController) {}
+
+  async favoriteAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      message: 'Deseja favoritar <strong>skater</strong> ?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        },
+        {
+          text: 'Sim, favoritar!',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
 }
